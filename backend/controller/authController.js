21c23b86bd89fe091ffc4f22a7d0ee3eba6a9148
rishabhsrivastava956070-lab <rishabhsrivastava956070@gmail.com>
2025-11-30 15:15:26 +0530

    add

--- backend/controller/authController.js
+++ backend/controller/authController.js
@@ -89,5 +89,17 @@ export const login = async (req, res) => {
             token,
         });
     } catch (error) {
+        console.log("login error", error);
+        return res.status(500).json({ message: `login error: ${error.message}` });
+    }
+    }
+
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie("token");
+        return res.status(200).json({ message: "Logged out successfully" });
+    } catch (error) {
+        console.log("Logout error", error);
+        return res.status(500).json({ message: `Logout error: ${error.message}` });
     }
 }
--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import {login,registration } from "../controller/authController.js";
+import {login,logout,registration } from "../controller/authController.js";
 
 const authRoutes = express.Router();   // Create router
 
 authRoutes.post("/registration", registration);  // Use authRoutes
 authRoutes.post("/login", login);
+authRoutes.post("/logout", logout);  // Add logout route
 export default authRoutes;  // Export the same
